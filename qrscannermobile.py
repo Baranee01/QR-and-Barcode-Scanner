72d244f6aaeb861b760ff3ea3cb14e8c013d3f5e
Baraneetharan P S <80042740+Baranee01@users.noreply.github.com>
2021-05-17 21:38:52 +0530

    Add files via upload

---
+++ qrscannermobile.py
@@ -0,0 +1,43 @@
+#IMPORTING PACKAGES
+import numpy as np
+import cv2
+import requests
+import imutils
+import warnings
+warnings.filterwarnings('ignore')
+from pyzbar.pyzbar import decode
+
+#GETTING VIDEO FROM MOBILE
+#REPLACE THE BELOW URL WITH YOUR QWN. MAKE SURE TO ADD "/shot.jpg" AT LAST.
+url = "https://192.168.1.3:8080/shot.jpg"
+video = cv2.VideoCapture(url)
+
+#FETCHING VIDEO CONTINUOUSLY FROM THE URL
+while True:
+    image_resp = requests.get(url,verify = False)
+    image_arr = np.array(bytearray(image_resp.content), dtype=np.uint8)
+    image = cv2.imdecode(image_arr, -1)
+    image = imutils.resize(image, width=720, height=1080)
+
+    #DECODING THE IMAGE
+    for qrcode in decode(image):
+        mydata = qrcode.data.decode("UTF-8")
+        print(mydata)
+
+        #DETECTING QR CODE USING OUTLINE
+        points = np.array([qrcode.polygon], np.int64)
+        points = points.reshape((-1, 1, 2))
+        cv2.polylines(image, [points], True, (0, 255, 0), 5)
+    
+    #DISPLAYING WINDOW
+    cv2.imshow("QR Code Scanner", image)
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+#CLOSING ALL WINDOWS
+cv2.destroyAllWindows()
+
+    
+    
+
+    
